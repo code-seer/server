e4dc2bd09fab3aede7a8c6e9b27267764610b0f5
Bizuwork Melesse <bmelesse@elifsis.com>
2020-08-24 23:37:53 -0400

    Use environment variables in git_push.sh

--- codegen/script/git_push.sh
+++ codegen/script/git_push.sh
@@ -2,26 +2,28 @@
 # Push TypeScript files to the codegen repository
 
 archive=codegen-1.0-SNAPSHOT-typescript.zip
-git_remote=git@bitbucket.org:modern-lms/codegen.git
-commit_message="Add Course Service codegen files"
+#ar=${DEPLOY_ENV:-default_value}
+#git_remote=git@bitbucket.org:modern-lms/codegen.git
+#commit_message="Add Course Service codegen files"
 
 cd ../target
-git clone git@bitbucket.org:modern-lms/codegen.git
+#git clone git@bitbucket.org:modern-lms/codegen.git
+git clone "${GIT_REMOTE}"
+
 
 # Move the files to be pushed
-
-cd codegen && rm -rf artifacts/course
-mkdir -p artifacts/course
-cp ../../target/$archive artifacts/course
+cd codegen && rm -rf "$ARTIFACT_DIR"
+mkdir -p "$ARTIFACT_DIR"
+cp ../../target/$archive "$ARTIFACT_DIR"
 
 # Adds the files in the local repository and stages them for commit.
 git add .
 
 # Commits the tracked changes and prepares them to be pushed to a remote repository.
-git commit -m "$commit_message"
+git commit -m "${COMMIT_MESSAGE}"
 
 # Pushes (Forces) the changes in the local repository up to the remote repository
-echo "Git pushing to ${git_remote}"
+echo "Git pushing to ${GIT_REMOTE}"
 git push origin master
 
 
