fe49160cd35453b706d4cf0f63ce3922f8304fda
Bizuwork Melesse <bmelesse@elifsis.com>
2020-09-15 08:11:58 -0400

    Build a fat jar for multi-module project

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,7 @@
 
 mvn clean install -D skipTests
 
-./wait-for-it.sh -t 0 "${CONFIG_SERVER_HOST}":"${CONFIG_SERVER_PORT}"
-./wait-for-it.sh -t 0 "${SERVICE_DISCOVERY_HOST}":"${SERVICE_DISOVERY_PORT}"
 ./wait-for-it.sh -t 0 "${API_GATEWAY_HOST}":"${API_GATEWAY_PORT}"
-./wait-for-it.sh -t 0 "${DB_HOST}":"${DB_PORT}"
-./wait-for-it.sh -t 0 "${ELASTICSEARCH_HOST}":"${ELASTICSEARCH_PORT}"
 
 # Change working directory to run mvn spring-boot:run
 cd api
