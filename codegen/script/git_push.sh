e1890f7b3af61d05773d169e09409f42316f8f1b
Bizuwork Melesse <bmelesse@elifsis.com>
2020-09-17 21:58:14 -0400

    Remove old commands from git_push.sh

--- codegen/script/git_push.sh
+++ codegen/script/git_push.sh
@@ -5,9 +5,6 @@ then
   # Push TypeScript files to the codegen repository
 
   archive=codegen-1.0-SNAPSHOT-typescript.zip
-  #ar=${DEPLOY_ENV:-default_value}
-  #git_remote=git@bitbucket.org:modern-lms/codegen.git
-  #commit_message="Add Course Service codegen files"
 
   cd ../target
   #git clone git@bitbucket.org:modern-lms/codegen.git
@@ -15,15 +12,15 @@ then
 
 
   # Move the files to be pushed
-  cd codegen && rm -rf "$ARTIFACT_DIR"
-  mkdir -p "$ARTIFACT_DIR"
-  cp ../../target/$archive "$ARTIFACT_DIR"
+  cd codegen && rm -rf "$CODEGEN_ARTIFACT_DIR"
+  mkdir -p "$CODEGEN_ARTIFACT_DIR"
+  cp ../../target/$archive "$CODEGEN_ARTIFACT_DIR"
 
   # Adds the files in the local repository and stages them for commit.
   git add .
 
   # Commits the tracked changes and prepares them to be pushed to a remote repository.
-  git commit -m "${COMMIT_MESSAGE}"
+  git commit -m "${GIT_COMMIT_MESSAGE}"
 
   # Pushes (Forces) the changes in the local repository up to the remote repository
   echo "Git pushing to ${GIT_REMOTE}"
