4e48103218133802c419d3d0a9d0190bdfac95bc
Bizuwork Melesse <bmelesse@elifsis.com>
2020-09-20 18:01:15 -0400

    Create RestHighLevelClient with constructor-injected values

--- search/src/main/kotlin/com/lms/modern/starter/search/SearchConfiguration.kt
+++ search/src/main/kotlin/com/lms/modern/starter/search/SearchConfiguration.kt
@@ -1,35 +1,33 @@
-//package com.lms.modern.starter.search
-//
-//import com.lms.modern.starter.config.SystemConfigConfiguration
-//import com.lms.modern.starter.config.api.SystemConfig
-//import com.lms.modern.starter.data.DataConfiguration
-//import com.lms.modern.starter.kafka.KafkaConfiguration
-//import com.lms.modern.starter.util.UtilConfiguration
-//import org.apache.http.HttpHost
-//import org.elasticsearch.client.RestClient
-//import org.elasticsearch.client.RestHighLevelClient
-//import org.springframework.beans.factory.annotation.Autowired
-//import org.springframework.context.annotation.Bean
-//import org.springframework.context.annotation.ComponentScan
-//import org.springframework.context.annotation.Configuration
-//import org.springframework.context.annotation.Import
-//
-//
-//@Configuration
-//@ComponentScan
-//@Import(value = [
-//    DataConfiguration::class,
-//    SystemConfigConfiguration::class,
-//    KafkaConfiguration::class,
-//    UtilConfiguration::class])
-//class SearchConfiguration {
-//
-//    @Autowired
-//    lateinit var config: SystemConfig
-//
-//    @Bean(destroyMethod = "close")
-//    fun client(): RestHighLevelClient {
-//        return RestHighLevelClient(
-//                RestClient.builder(HttpHost(config.elasticsearchHost, config.elasticsearchPort)))
-//    }
-//}
+package com.lms.modern.starter.search
+
+import com.lms.modern.starter.data.DataConfiguration
+import com.lms.modern.starter.util.UtilConfiguration
+import org.apache.http.HttpHost
+import org.elasticsearch.client.RestClient
+import org.elasticsearch.client.RestHighLevelClient
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+
+
+@Configuration
+@ComponentScan
+@Import(value = [
+    DataConfiguration::class,
+    UtilConfiguration::class])
+class SearchConfiguration(
+    @Value("\${elasticsearch.host: ---Elasticsearch host not found---}")
+    open var elasticsearchHost: String,
+
+    @Value("\${elasticsearch.port: 9200}")
+    open var elasticsearchPort: Int
+) {
+
+    @Bean(destroyMethod = "close")
+    open fun client(): RestHighLevelClient {
+        return RestHighLevelClient(
+                RestClient.builder(HttpHost(elasticsearchHost, elasticsearchPort)))
+    }
+}
