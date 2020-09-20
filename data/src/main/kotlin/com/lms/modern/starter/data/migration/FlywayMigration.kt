790e682172b8e0f70400fd4192b0345324c82272
Bizuwork Melesse <bmelesse@elifsis.com>
2020-09-20 13:50:27 -0400

    Implement index deletion

--- data/src/main/kotlin/com/lms/modern/starter/data/migration/FlywayMigration.kt
+++ data/src/main/kotlin/com/lms/modern/starter/data/migration/FlywayMigration.kt
@@ -11,4 +11,22 @@ interface FlywayMigration {
      * Drop database. Not allowed in production.
      */
     fun clean()
+
+    /**
+     * Delete all Elasticserch indices.
+     */
+    fun deleteIndex()
+
+    /**
+     * Bulk index new migration for Elasticsearch.
+     */
+//    fun index()
+//    private val threadCount = 5
+//    override fun index() {
+//        log.info("Bulk indexing")
+//        val searchSession = Search.session(entityManager)
+//        val indexer: MassIndexer = searchSession.massIndexer(DemoUserEntity::class.java)
+//                .threadsToLoadObjects(threadCount)
+//        indexer.startAndWait()
+//    }
 }
--- data/src/main/kotlin/com/lms/modern/starter/data/migration/FlywayMigrationImpl.kt
+++ data/src/main/kotlin/com/lms/modern/starter/data/migration/FlywayMigrationImpl.kt
@@ -2,13 +2,25 @@ package com.lms.modern.starter.data.migration
 
 import com.lms.modern.starter.data.DataConfiguration
 import com.lms.modern.starter.util.lib.line
+import org.elasticsearch.action.admin.indices.cache.clear.ClearIndicesCacheRequest
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest
+import org.elasticsearch.client.RequestOptions
+import org.elasticsearch.client.RestHighLevelClient
+import org.elasticsearch.client.indices.GetIndexRequest
 import org.flywaydb.core.Flyway
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
+
+/**
+ * Migrate schema and performs bulk indexing for Elasticsearch.
+ */
 @Component
-class FlywayMigrationImpl(private val dataConfig: DataConfiguration): FlywayMigration {
+class FlywayMigrationImpl(
+        private val dataConfig: DataConfiguration,
+        private val client: RestHighLevelClient
+): FlywayMigration {
 
     private val log: Logger = LoggerFactory.getLogger(this.javaClass)
     private val maxNameLength = 60
@@ -30,12 +42,35 @@ class FlywayMigrationImpl(private val dataConfig: DataConfiguration): FlywayMigr
     }
 
     override fun clean() {
-        if (dataConfig.profile == "dev") {
+        if (dataConfig.profile == "dev" || dataConfig.applicationName == "starter") {
             log.info(String.format("  | %-" + maxNameLength.toString() + "s : %s", "Dropping database", "true"))
             Flyway(flyway).clean()
+            deleteIndex()
         } else {
             log.info(String.format("  | %-" + maxNameLength.toString() + "s : %s", "Dropping database", "false"))
         }
+    }
+
+    override fun deleteIndex() {
+        log.info("  +$line")
+        log.info("  | Elasticsearch Index Deletion")
+        log.info("  +$line")
+        val indices: Array<String> = client.indices().get(GetIndexRequest("*"), RequestOptions.DEFAULT).indices
+        indices.sort()
+        val deletedIndices: MutableList<String> = ArrayList()
+        for (index in indices) {
+            if (index.startsWith("${dataConfig.indexPrefix}-")) {
+                val clearCacheRequest = ClearIndicesCacheRequest(index)
+                val deleteRequest = DeleteIndexRequest(index)
+                client.indices().clearCache(clearCacheRequest, RequestOptions.DEFAULT)
+                val ack = client.indices().delete(deleteRequest, RequestOptions.DEFAULT)
+                if (ack.isAcknowledged) {
+                    log.info(String.format("  | %-" + maxNameLength.toString() + "s : %s", index, "deleted"))
+                    deletedIndices.add(index)
+                }
+            }
+        }
+        log.info(String.format("  | %-" + maxNameLength.toString() + "s : %s", "Num deleted", deletedIndices.size))
     }
 
 }
