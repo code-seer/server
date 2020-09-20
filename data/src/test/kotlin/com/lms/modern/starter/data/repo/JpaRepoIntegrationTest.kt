4ad8a997cb1ac48cb487a86556c3642716025bc2
Bizuwork Melesse <bmelesse@elifsis.com>
2020-09-20 15:39:05 -0400

    Compare entity equality and assert

--- data/src/test/kotlin/com/lms/modern/starter/data/repo/JpaRepoIntegrationTest.kt
+++ data/src/test/kotlin/com/lms/modern/starter/data/repo/JpaRepoIntegrationTest.kt
@@ -9,9 +9,9 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests
 import org.springframework.transaction.annotation.Transactional
-import org.testng.annotations.AfterClass
 import org.testng.annotations.BeforeClass
 import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
@@ -33,18 +33,9 @@ open class JpaRepoIntegrationTest: AbstractTestNGSpringContextTests() {
     @Autowired
     lateinit var flywayMigration: FlywayMigration
 
-//    @Autowired
-//    lateinit var entityMapper: EntityMapper
-
     @BeforeClass
     fun beforeClass() {
-        flywayMigration.clean()
         flywayMigration.init()
-    }
-
-    @AfterClass
-    open fun afterClass() {
-//        flywayMigration.clean()
     }
 
     @BeforeMethod
@@ -63,150 +54,45 @@ open class JpaRepoIntegrationTest: AbstractTestNGSpringContextTests() {
     @Test(priority = 1)
     open fun testCreate() {
         val faker = Faker()
-        val entity = DemoUserEntity()
+        var expected = DemoUserEntity()
         val now = OffsetDateTime.now()
-//        entity.id =
-        entity.firstName = faker.name().firstName()
-        entity.lastName = faker.name().lastName()
-        entity.city = faker.address().city()
-        entity.state = faker.address().state()
-        entity.zip = faker.address().zipCode()
-        entity.address = faker.address().fullAddress()
-        entity.favorites = arrayOf(
+        expected.firstName = faker.name().firstName()
+        expected.lastName = faker.name().lastName()
+        expected.city = faker.address().city()
+        expected.state = faker.address().state()
+        expected.zip = faker.address().zipCode()
+        expected.address = faker.address().fullAddress()
+        expected.favorites = arrayOf(
                 faker.starTrek().specie(),
                 faker.starTrek().specie(),
                 faker.starTrek().specie())
-        entity.avatar = faker.avatar().image()
-        entity.createdDt = now
-        entity.updatedDt = now
-        entity.uuid = UUID.randomUUID()
-        demoUserRepo.save(entity)
-
-//        val expected = getEntity()
-//        val saved = demoUserRepo.save(expected)
-
-        // Save multiple copies to test that transactions are rolled back. If rollback fails,
-        // testFindAll() should fail
-//        demoUserRepo.save(getEntity())
-//        demoUserRepo.save(getEntity())
-//        demoUserRepo.save(getEntity())
-//        demoUserRepo.save(getEntity())
-//
-//        val response = saved.id?.let { demoUserRepo.findById(it) }
-//        assertNotNull(response)
-//        assertEquals(false, response.isEmpty)
-//        val actual = response.get()
-//        assertEntityEquality(expected, actual)
+        expected.avatar = faker.avatar().image()
+        expected.createdDt = now
+        expected.updatedDt = now
+        expected.uuid = UUID.randomUUID()
+        expected = demoUserRepo.save(expected)
+        val actual = demoUserRepo.findByIdOrNull(expected.id)
+        assertNotNull(actual)
+        assertEntityEquality(expected, actual)
     }
 
-//    @Test
-//    open fun testUpdate() {
-//        // Create a new entity and save it
-//        val expected = getEntity()
-//        val saved = demoUserRepo.save(expected)
-//        var response = saved.id?.let { demoUserRepo.findById(it) }
-//        assertNotNull(response)
-//        assertEquals(false, response.isEmpty)
-//        val actual = response.get()
-//        assertEntityEquality(expected, actual)
-//
-//        // Update the entity
-//        val toUpdate = entityMapper.map(actual)
-//        toUpdate.status = "Active"
-//        toUpdate.location = "Chicago"
-//        toUpdate.updatedDt = OffsetDateTime.now()
-//        demoUserRepo.save(toUpdate)
-//        response = saved.id?.let { demoUserRepo.findById(it) }
-//        assertNotNull(response)
-//        assertEquals(false, response.isEmpty)
-//        val updated = response.get()
-//        assertEntityEquality(toUpdate, updated)
-//    }
 
-//    @Test
-//    open fun testDelete() {
-//        val expected = getEntity()
-//        val saved = demoUserRepo.save(expected)
-//        var response = saved.id?.let { demoUserRepo.findById(it) }
-//        assertNotNull(response)
-//        assertEquals(false, response.isEmpty)
-//        var actual = response.get()
-//        assertEntityEquality(expected, actual)
-//
-//        // Delete the entity
-//        demoUserRepo.delete(actual)
-//        response = saved.id?.let { demoUserRepo.findById(it) }
-//        assertNotNull(response)
-//        assertEquals(response.isEmpty, true)
-//    }
-
-//    @Test
-//    open fun testFindById() {
-//        var response = demoUserRepo.findById(1)
-//        assertNotNull(response)
-//        assertEquals(false, response.isEmpty)
-//        var actual = response.get()
-//        assertEquals("Gamification", actual.name)
-//        assertEquals("Absolute Beginner", actual.level)
-//        assertEquals("G_oY", actual.shortName)
-//        assertEquals(399, actual.price)
-//        assertEquals("Contest slippered insular central merged", actual.learningObjectives[4])
-//    }
-
-    private fun getEntity(): DemoUserEntity {
-        val requirements = arrayOf("Alpha req", "Beta req", "Gamma req")
-        val learningObjectives = arrayOf("Alpha objective", "Beta objective", "Gamma objective")
-        val skills = arrayOf("Alpha skill", "Beta skill", "Gamma skill")
-        val entity = DemoUserEntity()
-        val now = OffsetDateTime.now()
-//        entity.id = null
-//        entity.status = "Draft"
-//        entity.level = "Introductory"
-//        entity.subject = "Computer Science,Root.Engineering.Computer_Science"
-//        entity.instructionMode = "Offline"
-//        entity.name = "Spring Boot Test course"
-//        entity.description = "This is a test course created for testing Spring Data integration"
-//        entity.requirements = requirements
-//        entity.location =  "Berlin"
-//        entity.numCredits = 4
-//        entity.price = 99
-//        entity.weeklyEffort = 8
-//        entity.avatar = "avatar not available"
-//        entity.learningObjectives = learningObjectives
-//        entity.shortName = "SBTC"
-//        entity.skills = skills
-        entity.createdDt = now
-        entity.updatedDt = now
-        entity.uuid = UUID.randomUUID()
-        return entity
+    /**
+     * Manually compare entity fields and assert equality.
+     *
+     * Although ReflectionEquals is more concise, it doesn't assert individual fields.
+     */
+    private fun assertEntityEquality(expected: DemoUserEntity, actual: DemoUserEntity) {
+        assertEquals(expected.id, actual.id)
+        assertEquals(expected.firstName, actual.firstName)
+        assertEquals(expected.lastName, actual.lastName)
+        assertEquals(expected.city, actual.city)
+        assertEquals(expected.state, actual.state)
+        assertEquals(expected.zip, actual.zip)
+        assertEquals(expected.address, actual.address)
+        assertEquals(expected.avatar, actual.avatar)
+        assertEquals(expected.createdDt, actual.createdDt)
+        assertEquals(expected.updatedDt, actual.updatedDt)
+        assertEquals(expected.uuid, actual.uuid)
     }
-//
-//    /**
-//     * Manually compare entity fields and assert equality.
-//     *
-//     * Although ReflectionEquals is more concise, it doesn't assert individual fields.
-//     */
-//    private fun assertEntityEquality(expected: DemoEntity, actual: DemoEntity) {
-//        assertEquals(expected.id, actual.id)
-//        assertEquals(expected.status, actual.status)
-//        assertEquals(expected.level, actual.level)
-//        assertEquals(expected.subject, actual.subject)
-//        assertEquals(expected.instructionMode, actual.instructionMode)
-//        assertEquals(expected.name, actual.name)
-//        assertEquals(expected.description, actual.description)
-//        assertEquals(expected.requirements contentEquals actual.requirements, true)
-//        assertEquals(expected.location, actual.location)
-//        assertEquals(expected.numCredits, actual.numCredits)
-//        assertEquals(expected.price, actual.price)
-//        assertEquals(expected.weeklyEffort, actual.weeklyEffort)
-//        assertEquals(expected.avatar, actual.avatar)
-//        assertEquals(expected.learningObjectives contentEquals actual.learningObjectives, true)
-//        assertEquals(expected.shortName, actual.shortName)
-//        assertEquals(expected.skills contentEquals actual.skills, true)
-//        assertEquals(expected.startTimeDt, actual.startTimeDt)
-//        assertEquals(expected.endTimeDt, actual.endTimeDt)
-//        assertEquals(expected.createdDt, actual.createdDt)
-//        assertEquals(expected.updatedDt, actual.updatedDt)
-//        assertEquals(expected.uuid, actual.uuid)
-//    }
 }
