3f6fb225159cd64cc07cfc7c1a6e9dfba75d312c
Bizuwork Melesse <bmelesse@elifsis.com>
2020-09-24 23:23:08 -0400

    Test google credential parser

--- security/src/main/kotlin/com/lms/modern/starter/security/SecurityConfiguration.kt
+++ security/src/main/kotlin/com/lms/modern/starter/security/SecurityConfiguration.kt
@@ -1,9 +1,12 @@
 package com.lms.modern.starter.security
 
+import com.lms.modern.starter.config.SystemConfigConfiguration
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 
 @Configuration
 @ComponentScan
+@Import(value = [SystemConfigConfiguration::class])
 class SecurityConfiguration {
 }
