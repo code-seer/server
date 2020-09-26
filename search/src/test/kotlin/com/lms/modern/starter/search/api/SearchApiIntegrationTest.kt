306d9b9a365b250e08ec5bc6b888a54568a349c4
Bizuwork Melesse <bmelesse@elifsis.com>
2020-09-26 17:16:20 -0400

    Test sorting in search response

--- search/src/test/kotlin/com/lms/modern/starter/search/api/SearchApiIntegrationTest.kt
+++ search/src/test/kotlin/com/lms/modern/starter/search/api/SearchApiIntegrationTest.kt
@@ -9,6 +9,7 @@ import org.elasticsearch.action.search.SearchRequest
 import org.elasticsearch.client.core.CountRequest
 import org.elasticsearch.index.query.QueryBuilders
 import org.elasticsearch.search.builder.SearchSourceBuilder
+import org.elasticsearch.search.sort.SortOrder
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -62,14 +63,14 @@ class SearchApiIntegrationTest: AbstractTestNGSpringContextTests() {
     }
 
     @Test
-    fun getDemoUserCount() {
+    fun demo_user_count_test() {
         val countRequest = CountRequest(arrayOf(testIndex), QueryBuilders.matchAllQuery())
         val response = searchApi.count(countRequest)
         assertEquals( 100, response.count)
     }
 
     @Test
-    fun findById() {
+    fun find_by_id_test() {
         val searchRequest = SearchRequest(testIndex)
         val sourceBuilder =  SearchSourceBuilder()
         sourceBuilder.query(QueryBuilders.termQuery("id", 23))
@@ -84,5 +85,26 @@ class SearchApiIntegrationTest: AbstractTestNGSpringContextTests() {
         assertEquals(firstName, page.records[0].firstName)
         assertEquals(avatar, page.records[0].avatar)
         assertEquals(true, page.records[0].address.contains(address))
+    }
+
+    @Test
+    fun find_all_sort_test() {
+        val searchRequest = SearchRequest(testIndex)
+        val sourceBuilder =  SearchSourceBuilder()
+        sourceBuilder.query(QueryBuilders.matchAllQuery())
+        val offset = 0
+        val size = 25
+        sourceBuilder
+                .sort("id", SortOrder.ASC)
+                .size(size)
+                .from(offset)
+                .query(QueryBuilders.matchAllQuery())
+        searchRequest.source(sourceBuilder)
+        val page = LmsPage(searchApi.execute(searchRequest), offset, size, DemoUserDto::class.java, objectMapper)
+        assertNotNull(page)
+        assertEquals(100, page.totalRecords)
+        assertEquals( 4, page.numPages)
+        assertEquals( "Roxann", page.records[6].firstName)
+        assertEquals( "Walter", page.records[6].lastName)
     }
 }
