43ed2d8f9672f9a8113ca144d6845dd8b434e49d
Bizuwork Melesse <bmelesse@elifsis.com>
2020-09-27 09:42:09 -0400

    Implement security filter with Firebase idToken and user claims

--- api/src/main/kotlin/com/lms/modern/starter/api/security/SecurityFilter.kt
+++ api/src/main/kotlin/com/lms/modern/starter/api/security/SecurityFilter.kt
@@ -1,6 +1,13 @@
 package com.lms.modern.starter.api.security
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseToken
+import com.lms.modern.starter.model.UserDto
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.stereotype.Component
 import org.springframework.util.StringUtils
 import org.springframework.web.filter.OncePerRequestFilter
@@ -11,21 +18,25 @@ import javax.servlet.http.HttpServletResponse
 @Component
 class SecurityFilter: OncePerRequestFilter() {
 
-//    @Throws(ServletException::class, IOException::class)
+    @Autowired
+    private lateinit var securityService: SecurityService
+
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
         verifyToken(request)
         filterChain.doFilter(request, response)
     }
 
     private fun verifyToken(request: HttpServletRequest) {
-        var bearerToken: String? = null
-        val authorization = request.getHeader("Authorization")
-        if (StringUtils.hasText(authorization) && authorization.startsWith("Bearer ")) {
-            bearerToken = authorization.substring(7, authorization.length)
-        }
+        var bearerToken: String = securityService.getBearerToken(request)
         val decodedToken = FirebaseAuth.getInstance().verifyIdToken(bearerToken)
-//        decodedToken
-        val x = 1
+        val userDto = firebaseTokenToUserDto(decodedToken)
+        val authentication = UsernamePasswordAuthenticationToken(userDto,
+                Credentials(Credentials.CredentialType.ID_TOKEN, decodedToken, bearerToken),
+                securityService.getAuthorities(userDto.roles))
+        authentication.details = WebAuthenticationDetailsSource().buildDetails(request)
+        SecurityContextHolder.getContext().authentication = authentication
+
+
 //        var sessionCookieValue: String? = null
 //        var decodedToken: FirebaseToken? = null
 //        var type: CredentialType? = null
@@ -57,17 +68,16 @@ class SecurityFilter: OncePerRequestFilter() {
 //        }
     }
 
-//    private fun firebaseTokenToUserDto(decodedToken: FirebaseToken?): User? {
-//        var user: User? = null
-//        if (decodedToken != null) {
-//            user = User()
-//            user.setUid(decodedToken.uid)
-//            user.setName(decodedToken.name)
-//            user.setEmail(decodedToken.email)
-//            user.setPicture(decodedToken.picture)
-//            user.setIssuer(decodedToken.issuer)
-//            user.setEmailVerified(decodedToken.isEmailVerified)
-//        }
-//        return user
-//    }
+    private fun firebaseTokenToUserDto(decodedToken: FirebaseToken?): UserDto {
+        val userDto = UserDto()
+        if (decodedToken != null) {
+            userDto.uid = decodedToken.uid
+            userDto.name = decodedToken.name
+            userDto.email = decodedToken.email
+            userDto.picture = decodedToken.picture
+            userDto.isEmailVerified(decodedToken.isEmailVerified)
+            userDto.roles = decodedToken.claims
+        }
+        return userDto
+    }
 }
