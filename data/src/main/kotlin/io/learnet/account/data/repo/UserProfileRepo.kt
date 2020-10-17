878f8664ffd0788e230bc4fc11a0ef8365bdeb2e
Bizuwork Melesse <bmelesse@elifsis.com>
2020-10-16 21:28:23 -0400

    Define UserProfileEntity against user_profile

---
+++ data/src/main/kotlin/io/learnet/account/data/repo/UserProfileRepo.kt
@@ -0,0 +1,10 @@
+package io.learnet.account.data.repo
+
+import io.learnet.account.data.entity.UserProfileEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
+
+@Repository
+@Transactional
+interface UserProfileRepo : JpaRepository<UserProfileEntity, Long>
