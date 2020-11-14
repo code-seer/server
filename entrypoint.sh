3af24864536486cc3d66208b3c69ca8b99db8326
Biz Melesse <bizmelesse@gmail.com>
2020-11-13 21:58:29 -0500

    Delete docker-related files

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./wait-for-it.sh -t 0 "${API_GATEWAY_HOST}":"${API_GATEWAY_PORT}"
-./wait-for-it.sh -t 0 "${ELASTICSEARCH_HOST}":"${ELASTICSEARCH_PORT}"
-
-# now run the requested CMD without forking a subprocess
-exec "$@"
