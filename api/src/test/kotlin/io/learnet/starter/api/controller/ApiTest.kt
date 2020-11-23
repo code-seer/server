7c8d2047ebdc6532ef0975e77d94b68c9e703424
Biz Melesse <bizmelesse@gmail.com>
2020-11-22 19:51:35 -0500

    Fix test url

--- api/src/test/kotlin/io/learnet/starter/api/controller/ApiTest.kt
+++ api/src/test/kotlin/io/learnet/starter/api/controller/ApiTest.kt
@@ -53,7 +53,7 @@ class ApiTest: AbstractTestNGSpringContextTests() {
     @Qualifier("jacksonObjectMapper")
     lateinit var objectMapper: ObjectMapper
 
-    private val testUrl = "/api/starter/demoUser/findAllUsers"
+    private val testUrl = "/starter/demoUser/findAllUsers"
     private val host = "localhost"
     private var idToken: String? = null
     private var userRecord: UserRecord? = null
