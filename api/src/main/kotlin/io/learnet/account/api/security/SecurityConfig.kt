907fb478d358f43326d0e6f36981b66f549bc4d2
Biz Melesse <bizmelesse@gmail.com>
2020-11-28 19:42:43 -0500

    Remove non-existent SecurityProps import

--- api/src/main/kotlin/io/learnet/account/api/security/SecurityConfig.kt
+++ api/src/main/kotlin/io/learnet/account/api/security/SecurityConfig.kt
@@ -1,7 +1,6 @@
 package io.learnet.account.api.security
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import io.learnet.account.util.properties.SecurityProps
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
