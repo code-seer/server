ce78abd9f6018a4572b5c7259ed823efa3a40947
Biz Melesse <bizmelesse@gmail.com>
2020-12-11 18:11:02 -0500

    Test permission creation service

--- service/src/test/kotlin/io/learnet/account/service/ServiceTestConfiguration.kt
+++ service/src/test/kotlin/io/learnet/account/service/ServiceTestConfiguration.kt
@@ -1,10 +1,11 @@
 package io.learnet.account.service
 
+import io.learnet.account.util.FirebaseConfig
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.Import
 
 @SpringBootConfiguration
 @EnableAutoConfiguration
-@Import(value = [ServiceConfiguration::class])
+@Import(value = [ServiceConfiguration::class, FirebaseConfig::class])
 class ServiceTestConfiguration
---
+++ service/src/test/kotlin/io/learnet/account/service/api/user/UserManagementTest.kt
@@ -0,0 +1,86 @@
+package io.learnet.account.service.api.user
+
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.UserRecord
+import io.learnet.account.model.UserPermissionsRequest
+import io.learnet.account.service.ServiceTestConfiguration
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests
+import org.testng.annotations.*
+import java.lang.reflect.Method
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+@SpringBootTest(classes = [ServiceTestConfiguration::class])
+class UserManagementTest: AbstractTestNGSpringContextTests() {
+
+    private val log: Logger = LoggerFactory.getLogger(this.javaClass)
+    private val testEmail = "learnetuser@learnet.io"
+
+    @Autowired
+    lateinit var userManagement: UserManagement
+
+    @BeforeClass
+    fun beforeClass() {
+        log.info("Test class ${this.javaClass.simpleName} started")
+        deleteUser()
+        val request: UserRecord.CreateRequest = UserRecord.CreateRequest()
+                .setEmail(testEmail)
+                .setEmailVerified(false)
+                .setDisabled(false)
+        FirebaseAuth.getInstance().createUser(request)
+        log.info("Test user with email address $testEmail has been created")
+    }
+
+    @AfterClass
+    fun afterClass() {
+        deleteUser()
+        log.info("Test class ${this.javaClass.simpleName} ended")
+    }
+
+    @BeforeMethod
+    fun beforeMethod(method: Method) {
+        log.info("Test method ${method.name} started")
+    }
+
+    @AfterMethod
+    fun afterMethod(method: Method) {
+        log.info("Test method ${method.name} ended")
+    }
+
+    private fun deleteUser() {
+        try {
+            FirebaseAuth.getInstance().deleteUser(FirebaseAuth.getInstance().getUserByEmail(testEmail).uid)
+            log.info("Test user with email address $testEmail has been deleted")
+        } catch (e: FirebaseAuthException) {
+            log.info("Test user with email address $testEmail does not exist")
+        }
+    }
+
+    @Test
+    fun createPermissions() {
+        val request = UserPermissionsRequest()
+        request.email(testEmail)
+        request.displayName("LearNet User")
+        val response = userManagement.createPermissions(request)
+        assertNotNull(response)
+        assertEquals("OK", response)
+
+        val expected = listOf(
+                "ROLE_DEMO_USER_READ",
+                "ROLE_VIEW_DASHBOARD",
+                "ROLE_CREATE_CONFERENCE").toSet()
+        val userRecord = FirebaseAuth.getInstance().getUserByEmail(testEmail)
+        assertNotNull(userRecord)
+        assertEquals(expected.size, userRecord.customClaims.size)
+        val actual = userRecord.customClaims.toList()
+        for (role in actual) {
+            assertEquals(true, expected.contains(role.first))
+        }
+    }
+}
