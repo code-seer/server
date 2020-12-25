8b8b35f45a4f254aee7b4f601acb78175fea4460
Biz Melesse <bizmelesse@gmail.com>
2020-12-25 16:29:39 -0600

    Fix DigitalOceanProps prop name casing

--- util/src/main/kotlin/io/learnet/starter/util/properties/DigitalOceanProps.kt
+++ util/src/main/kotlin/io/learnet/starter/util/properties/DigitalOceanProps.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConfigurationProperties("digitalOcean")
+@ConfigurationProperties("digitalocean")
 class DigitalOceanProps {
     lateinit var endpoint: String
     lateinit var url: String
