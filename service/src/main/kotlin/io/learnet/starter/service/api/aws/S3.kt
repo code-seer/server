c7d9502ae10b83767ccb90648f87a10ad1c370c5
Biz Melesse <bizmelesse@gmail.com>
2020-12-30 19:16:46 -0600

    Test creation of a bucket

--- service/src/main/kotlin/io/learnet/starter/service/api/aws/S3.kt
+++ service/src/main/kotlin/io/learnet/starter/service/api/aws/S3.kt
@@ -1,26 +1,24 @@
 package io.learnet.starter.service.api.aws
 
+import io.learnet.starter.util.properties.S3Props
+import org.springframework.stereotype.Service
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.regions.Region
-import software.amazon.awssdk.services.s3.model.PutObjectRequest
-import software.amazon.awssdk.services.s3.model.PutObjectResponse
-import java.io.File
-import software.amazon.awssdk.services.s3.model.S3Exception
+import software.amazon.awssdk.services.s3.model.*
 import java.io.IOException
 
-import java.io.FileInputStream
 import java.nio.ByteBuffer
 import java.util.*
 
-
-class S3 {
+@Service
+class S3(private val s3Props: S3Props) {
 
     private fun getS3Client(): S3Client {
-        val credentials = AwsBasicCredentials.create("test", "test")
+        val credentials = AwsBasicCredentials.create(s3Props.key, s3Props.secret)
         val credentialsProvider = StaticCredentialsProvider.create(credentials)
         return S3Client.builder()
             .credentialsProvider(credentialsProvider)
@@ -30,7 +28,20 @@ class S3 {
 
     fun uploadObject(bucketName: String, fileName: String): String {
         val s3Client = getS3Client()
+        createBucket(s3Client, bucketName)
         return putS3ObjectFromByteBuffer(s3Client, bucketName, "objectKey")
+    }
+
+    private fun createBucket(s3Client: S3Client, bucketName: String) {
+       try {
+           s3Client.headBucket(HeadBucketRequest.builder()
+               .bucket(bucketName)
+               .build())
+       } catch (e: NoSuchBucketException) {
+           s3Client.createBucket(CreateBucketRequest.builder()
+               .bucket(bucketName)
+               .build())
+       }
     }
 
     private fun putS3ObjectFromByteBuffer(s3: S3Client,
---
+++ service/src/test/kotlin/io/learnet/starter/service/api/aws/AWSTest.kt
@@ -0,0 +1,52 @@
+package io.learnet.starter.service.api.aws
+
+
+import io.learnet.starter.data.migration.FlywayMigration
+import io.learnet.starter.model.PageableRequest
+import io.learnet.starter.service.ServiceTestConfiguration
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests
+import org.testng.annotations.*
+import java.lang.reflect.Method
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+@SpringBootTest(classes = [ServiceTestConfiguration::class])
+class AWSTest: AbstractTestNGSpringContextTests() {
+
+    private val log: Logger = LoggerFactory.getLogger(this.javaClass)
+
+    @Autowired
+    lateinit var flywayMigration: FlywayMigration
+
+    @Autowired
+    lateinit var s3: S3
+
+    @BeforeClass
+    fun beforeClass() {
+
+    }
+
+    @BeforeMethod
+    fun beforeMethod(method: Method) {
+        log.info("Testing ${method.name}")
+    }
+
+    @Test
+    fun uploadObject() {
+        s3.uploadObject("learnet-test-bucket", "dummyFileName")
+    }
+//    @Test
+//    fun findAllUsers() {
+//        val pageableRequest = PageableRequest()
+//        pageableRequest.offset = 0
+//        pageableRequest.limit = 25
+//        val page = demoUserService.findAllUsers(pageableRequest)
+//        assertNotNull(page)
+//        assertEquals(100, page.totalRecords)
+//        assertEquals(4, page.numPages)
+//    }
+}
