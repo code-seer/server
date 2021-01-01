6bb8498d9ef1695858671600a13c19a8a8d87af7
Biz Melesse <bizmelesse@gmail.com>
2021-01-01 11:19:24 -0600

    Define user management interface methods

--- api/src/main/kotlin/io/learnet/account/api/controller/user/UserController.kt
+++ api/src/main/kotlin/io/learnet/account/api/controller/user/UserController.kt
@@ -1,19 +1,67 @@
 package io.learnet.account.api.controller.user
 
-import io.learnet.account.api.PermissionsApi
-import io.learnet.account.model.InlineResponse200
-import io.learnet.account.model.UserPermissionsRequest
+import io.learnet.account.api.*
+import io.learnet.account.model.*
 import io.learnet.account.service.api.user.UserManagement
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.multipart.MultipartFile
 
 @Controller
 class UserController(
         private val userManagement: UserManagement
-): PermissionsApi {
+): PermissionsApi, ProfileApi, AvatarApi, SettingsApi, SocialApi {
 
     override fun createUserPermissions(userPermissionsRequest: UserPermissionsRequest): ResponseEntity<InlineResponse200> {
         val response = userManagement.createPermissions(userPermissionsRequest)
         return ResponseEntity.ok(InlineResponse200().status(response))
+    }
+
+    override fun getUserProfile(email: String?): ResponseEntity<UserProfileDto> {
+        TODO("Not yet implemented")
+    }
+
+    override fun saveUserProfile(userProfileDto: UserProfileDto?): ResponseEntity<UserProfileDto> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAvatarUrl(email: String?): ResponseEntity<UserAvatarResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override fun uploadUserAvatar(avatar: MultipartFile?): ResponseEntity<UserAvatarResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUserLanguage(email: String?): ResponseEntity<UserLanguageDto> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUserNotificationSettings(email: String?): ResponseEntity<UserNotificationSettingsDto> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUserPrivacySettings(email: String?): ResponseEntity<UserPrivacySettingsDto> {
+        TODO("Not yet implemented")
+    }
+
+    override fun saveUserLanguage(userLanguageDto: UserLanguageDto?): ResponseEntity<UserLanguageDto> {
+        TODO("Not yet implemented")
+    }
+
+    override fun saveUserNotificationSettings(userNotificationSettingsDto: UserNotificationSettingsDto?): ResponseEntity<UserNotificationSettingsDto> {
+        TODO("Not yet implemented")
+    }
+
+    override fun saveUserPrivacySettings(userPrivacySettingsDto: UserPrivacySettingsDto?): ResponseEntity<UserPrivacySettingsDto> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUserSocial(email: String?): ResponseEntity<UserSocialDto> {
+        TODO("Not yet implemented")
+    }
+
+    override fun saveUserSocial(userSocialDto: UserSocialDto?): ResponseEntity<UserSocialDto> {
+        TODO("Not yet implemented")
     }
 }
--- service/src/main/kotlin/io/learnet/account/service/api/user/UserManagement.kt
+++ service/src/main/kotlin/io/learnet/account/service/api/user/UserManagement.kt
@@ -1,6 +1,7 @@
 package io.learnet.account.service.api.user
 
-import io.learnet.account.model.UserPermissionsRequest
+import io.learnet.account.model.*
+import org.springframework.web.multipart.MultipartFile
 
 interface UserManagement {
 
@@ -9,5 +10,39 @@ interface UserManagement {
      * user record object.
      */
     fun createPermissions(request: UserPermissionsRequest): String
+
+    fun getUserProfile(email: String): UserProfileDto
+
+    fun getAvatarUrl(email: String): UserAvatarResponse
+
+    fun getUserLanguage(email: String): UserLanguageDto
+
+    fun getUserNotificationSettings(email: String): UserNotificationSettingsDto
+
+    fun saveUserPrivacySettings(email: String): UserPrivacySettingsDto
+
+    fun getUserSocial(email: String): UserSocialDto
+
+    fun saveUserProfile(userProfileDto: UserProfileDto): UserProfileDto
+
+    fun uploadUserAvatar(avatar: MultipartFile): UserAvatarResponse
+
+    fun saveUserLanguage(userLanguageDto: UserLanguageDto): UserLanguageDto
+
+    fun saveUserNotificationSettings(userNotificationSettingsDto: UserNotificationSettingsDto):
+            UserNotificationSettingsDto
+
+    fun saveUserPrivacySettings(userPrivacySettingsDto: UserPrivacySettingsDto):
+            UserPrivacySettingsDto
+
+    fun saveUserSocial(userSocialDto: UserSocialDto): UserSocialDto
+
+
+
+
+
+
+
+
 
 }
