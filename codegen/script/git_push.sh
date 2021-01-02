b1ece2086cd34d6f1e13d231ad074df7265f2b9c
Biz Melesse <bizmelesse@gmail.com>
2021-01-01 18:22:11 -0600

    Deprecate git_push and upload artifacts to gitlab-maven

--- codegen/script/git_push.sh
+++ codegen/script/git_push.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
-
-if [ "$PUSH_TYPESCRIPT_FILES" ]
-then
-  # Push TypeScript files to the codegen repository
-
-  archive=codegen-1.0-SNAPSHOT-typescript.zip
-
-  cd ../target
-  git clone "${GIT_CODEGEN_REMOTE}"
-
-
-  # Move the files to be pushed
-  cd codegen && rm -rf "$CODEGEN_ARTIFACT_DIR"
-  mkdir -p "$CODEGEN_ARTIFACT_DIR"
-  cp ../../target/$archive "$CODEGEN_ARTIFACT_DIR"
-
-  # Adds the files in the local repository and stages them for commit.
-  git add .
-
-  # Commits the tracked changes and prepares them to be pushed to a remote repository.
-  git commit -m "${GIT_COMMIT_MESSAGE}"
-
-  # Pushes (Forces) the changes in the local repository up to the remote repository
-  echo "Git pushing to ${GIT_CODEGEN_REMOTE}"
-  git push origin master
-fi
+echo "git_push has been deprecated in favor of gitlab-maven"
+#if [ "$PUSH_TYPESCRIPT_FILES" ]
+#then
+#  # Push TypeScript files to the codegen repository
+#
+#  archive=codegen-1.0-SNAPSHOT-typescript.zip
+#
+#  cd ../target
+#  git clone "${GIT_REMOTE}"
+#
+#
+#  # Move the files to be pushed
+#  cd codegen && rm -rf "$CODEGEN_ARTIFACT_DIR"
+#  mkdir -p "$CODEGEN_ARTIFACT_DIR"
+#  cp ../../target/$archive "$CODEGEN_ARTIFACT_DIR"
+#
+#  # Adds the files in the local repository and stages them for commit.
+#  git add .
+#
+#  # Commits the tracked changes and prepares them to be pushed to a remote repository.
+#  git commit -m "${GIT_COMMIT_MESSAGE}"
+#
+#  # Pushes (Forces) the changes in the local repository up to the remote repository
+#  echo "Git pushing to ${GIT_REMOTE}"
+#  git push origin master
+#fi
 
 
