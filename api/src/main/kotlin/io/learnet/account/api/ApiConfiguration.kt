7cf84f5f5f21e0c8016104338548149cdca7a338
Biz Melesse <bizmelesse@gmail.com>
2021-01-03 18:21:04 -0600

    Disable demo user creation

--- api/src/main/kotlin/io/learnet/account/api/ApiConfiguration.kt
+++ api/src/main/kotlin/io/learnet/account/api/ApiConfiguration.kt
@@ -55,7 +55,7 @@ class ApiConfiguration(private val demoUserRepo: DemoUserRepo) {
     @EventListener
     fun onApplicationStart(event: ApplicationStartedEvent) {
         handleMigration()
-        createDemoUser()
+//        createDemoUser()
     }
 
     /**
