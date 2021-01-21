dd4ecc1fe2b0b4dbd1018676e6ff9202f3ca8a3a
Biz Melesse <bizmelesse@gmail.com>
2021-01-20 21:12:10 -0600

    Use maven current time to version codegen files

--- codegen/script/npm_publish.sh
+++ codegen/script/npm_publish.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-#export $(xargs < .env)
 
 echo "Publishing to NPM registry..."
 
 cd ../target/generated-resources/typescript
 
 # Set auth token
-npm config set '//gitlab.learnet.io/api/v4/projects/$CI_PROJECT_ID/packages/npm/:_authToken' "$CI_JOB_TOKEN"
+npm config set "//gitlab.learnet.io/api/v4/projects/$CI_PROJECT_ID/packages/npm/:_authToken" "$CI_JOB_TOKEN"
 
 # Set registry for @learnet group scope
 npm config set @learnet:registry https://gitlab.learnet.io/api/v4/projects/$CI_PROJECT_ID/packages/npm/
