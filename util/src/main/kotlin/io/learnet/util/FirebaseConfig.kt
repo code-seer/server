900cac5068b235f1265db19100de570b0b0a5d82
Biz Melesse <bizmelesse@gmail.com>
2021-04-09 22:42:15 -0500

    Use ObjectMapper to initialize Firebase credentials

--- util/src/main/kotlin/io/learnet/util/FirebaseConfig.kt
+++ util/src/main/kotlin/io/learnet/util/FirebaseConfig.kt
@@ -1,11 +1,12 @@
 package io.learnet.util
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import io.learnet.util.properties.FirebaseProps
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.configurationprocessor.json.JSONObject
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -17,6 +18,10 @@ class FirebaseConfig {
 
     @Autowired
     private lateinit var firebaseProps: FirebaseProps
+
+    @Autowired
+    @Qualifier("jacksonObjectMapper")
+    lateinit var objectMapper: ObjectMapper
 
     @Primary
     @Bean
@@ -30,18 +35,18 @@ class FirebaseConfig {
     }
 
      fun getFirebaseCredentials(): GoogleCredentials {
-        val jsonObject = JSONObject();
-        jsonObject.put("type", firebaseProps.type)
-        jsonObject.put("project_id", firebaseProps.projectId)
-        jsonObject.put("private_key_id", firebaseProps.privateKeyId)
-        jsonObject.put("private_key", firebaseProps.privateKey)
-        jsonObject.put("client_email", firebaseProps.clientEmail)
-        jsonObject.put("client_id", firebaseProps.clientId)
-        jsonObject.put("auth_uri", firebaseProps.authUri)
-        jsonObject.put("token_uri", firebaseProps.tokenUri)
-        jsonObject.put("auth_provider_x509_cert_url", firebaseProps.authProviderX509CertUrl)
-        jsonObject.put("client_x509_cert_url", firebaseProps.clientX509CertUrl)
-        val inputStream = jsonObject.toString().byteInputStream(Charsets.UTF_8)
+         val objectNode = objectMapper.createObjectNode()
+         objectNode.put("type", firebaseProps.type)
+         objectNode.put("project_id", firebaseProps.projectId)
+         objectNode.put("private_key_id", firebaseProps.privateKeyId)
+         objectNode.put("private_key", firebaseProps.privateKey)
+         objectNode.put("client_email", firebaseProps.clientEmail)
+         objectNode.put("client_id", firebaseProps.clientId)
+         objectNode.put("auth_uri", firebaseProps.authUri)
+         objectNode.put("token_uri", firebaseProps.tokenUri)
+         objectNode.put("auth_provider_x509_cert_url", firebaseProps.authProviderX509CertUrl)
+         objectNode.put("client_x509_cert_url", firebaseProps.clientX509CertUrl)
+         val inputStream = objectMapper.writeValueAsString(objectNode).byteInputStream(Charsets.UTF_8)
         return GoogleCredentials.fromStream(inputStream)
     }
 }
